974d2c3557b4f9ccec104e55b1da3c2d1d323b27
Kodai.T <fvbcj53523@yahoo.co.jp>
2025-05-22 23:26:19 +0900

    add edge & force-dynamic

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -1,5 +1,10 @@
 import ListClubs from "@/components/microCMS/List_clubs";
 
+
+export const runtime = 'edge';
+
+export const dynamic = 'force-dynamic'; 
+
 export default function Home() {
   return (
     <div className="bg-white w-10xl">
